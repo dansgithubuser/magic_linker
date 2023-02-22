29c45d2f8f701e5f07cf4bffbc0cc3eff2f9a17b
Dan <dansonlinepresence@gmail.com>
2023-02-21 20:31:16 -0500

    models first sketch

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    sns_topic_arn = models.TextField()
+
+class Command(models.Model):
+    name = models.TextField(primary_key=True)
+    invocation = models.TextField()
+
+class Permission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    command = models.ForeignKey(Command , on_delete=models.CASCADE)
+
+class Execution(models.Model):
+    command = models.TextField()
+    result = models.IntegerField(null=True)
+    otp = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True)
