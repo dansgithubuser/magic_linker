7fafb9103ebd015f1df4f7bf1e46e5da7482f6e3
Dan <dansonlinepresence@gmail.com>
2023-02-25 13:14:32 -0500

    execute & favicon

--- app/models.py
+++ app/models.py
@@ -1,21 +1,65 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
+import logging
+import secrets
+import string
+import subprocess
+
+logger = logging.getLogger()
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     sns_topic_arn = models.TextField()
 
+    def __str__(self):
+        return f'{self.user} {self.sns_topic_arn}'
+
 class Command(models.Model):
     name = models.TextField(primary_key=True)
     invocation = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+    def execute(self, request):
+        Execution.create(request, self.name, self.invocation)
+        # TODO: send email
+
 class Permission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    command = models.ForeignKey(Command , on_delete=models.CASCADE)
+    command = models.ForeignKey(Command, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} {self.command}'
 
 class Execution(models.Model):
-    command = models.TextField()
+    command_name = models.TextField(db_index=True)
+    command_invocation = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    user_email = models.TextField(db_index=True)
+    otp = models.TextField(db_index=True)
     result = models.IntegerField(null=True)
-    otp = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     completed_at = models.DateTimeField(null=True)
+
+    def create(request, command_name, command_invocation):
+        return Execution.objects.create(
+            command_name=command_name,
+            command_invocation=command_invocation,
+            user=request.user,
+            user_email=request.user.email,
+            otp=''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32)),
+        )
+
+    def complete(self):
+        try:
+            self.result = subprocess.run(self.command_invocation.split()).returncode
+        except Exception as e:
+            logger.error(f'Error when completing execution {self.id}: {e}')
+        self.completed_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.user_email} {self.command_name} {self.created_at} {self.completed_at} {self.result}'
--- app/views.py
+++ app/views.py
@@ -3,11 +3,70 @@ from . import models
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
+from datetime import timedelta
 import json
+
+def _message(request, message, status):
+    return render(
+        request,
+        'message.html',
+        {'message': message},
+        status=status,
+    )
 
 @login_required
 def home(request):
     commands = models.Command.objects.filter(permission__user=request.user)
-    return render(request, 'home.html', {'commands': commands})
+    executions = (
+        models.Execution.objects
+            .filter(user=request.user)
+            .order_by('-id')
+            [:10]
+    )
+    return render(request, 'home.html', {
+        'commands': commands,
+        'executions': executions,
+    })
 home.route = ''
+
+@login_required
+@require_http_methods(['POST'])
+def command_execute(request, name):
+    if not models.Permission.objects.get(user=request.user, command=name):
+        return HttpResponse(status=401)
+    hour_ago = timezone.now() - timedelta(hours=1)
+    prev = (
+        models.Execution.objects
+            .filter(
+                command_name=name,
+                user=request.user,
+                created_at__gte=hour_ago,
+                completed_at__isnull=True,
+            )
+            .first()
+    )
+    if prev:
+        return _message(request, 'Incomplete execution for this command already exists.', 429)
+    command = models.Command.objects.get(name=name)
+    command.execute(request)
+    return redirect('/')
+command_execute.route = 'command/<str:name>/execute'
+
+@login_required
+@csrf_exempt
+def execution_complete(request, otp):
+    execution = models.Execution.objects.get(otp=otp)
+    if not execution or execution.user != request.user:
+        return _message(request, 'No such execution.', 404)
+    if execution.completed_at:
+        return _message(request, 'Execution has already been completed.', 400)
+    hour_ago = timezone.now() - timedelta(hours=1)
+    if execution.created_at < hour_ago:
+        return _message(request, 'Execution has expired.', 400)
+    execution.complete()
+    return redirect('/')
+execution_complete.route = 'execution/<str:otp>/complete'
