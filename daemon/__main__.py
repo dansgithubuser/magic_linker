8faaba3fd6197b611610a3b541742c32071c914b
Dan <dansonlinepresence@gmail.com>
2023-07-08 16:46:19 -0400

    upkeep

--- daemon/__main__.py
+++ daemon/__main__.py
@@ -15,17 +15,29 @@ def timestamp():
 def tprint(*args, **kwargs):
     print(timestamp(), *args, **kwargs)
 
+def run(execution, kind):
+    execution = os.path.join(EXECUTIONS_DIR, execution)
+    with open(execution) as f:
+        args = f.read().split()
+    if args[0] in COMMANDS:
+        p = subprocess.Popen(['python3', *args], cwd=COMMANDS_DIR)
+        tprint(f'PID {p.pid} - start {kind} command `{args[0]}`')
+        code = p.wait()
+        tprint(f'PID {p.pid} - exited with code {code}')
+    os.remove(execution)
+
 while True:
     time.sleep(10)
     for execution in os.listdir(EXECUTIONS_DIR):
         if not execution.startswith('execution_'):
             continue
-        execution = os.path.join(EXECUTIONS_DIR, execution)
-        with open(execution) as f:
-            args = f.read().split()
-        if args[0] in COMMANDS:
-            p = subprocess.Popen(['python3', *args], cwd=COMMANDS_DIR)
-            tprint(f'PID {p.pid} - start command `{args[0]}`')
-            code = p.wait()
-            tprint(f'PID {p.pid} - exited with code {code}')
-        os.remove(execution)
+        kind = 'execution'
+        if execution.startswith('execution_upkeep_'):
+            try:
+                t = int(execution.split('_')[2])
+            except:
+                t = 0
+            if time.time() < t:
+                continue
+            kind = 'upkeep'
+        run(execution, kind)
